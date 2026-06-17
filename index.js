f8b6dbd55fd9fb17bfd8737eaf7cfd07559c50d5
Bernardo-Cardoso-vieira <bcv@aluno.ifnmg.edu.br>
2026-06-17 22:32:06 +0000

    refactor: exclusao dos arquivos temp-repl.js e temp-desafio.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express'; // importa o Express usando ES Modules
-
+import prisma from './prisma/client.js';
 const app = express(); // cria a aplicação Express
 const PORT = 3000; // porta onde o servidor vai rodar localmente
 
@@ -23,3 +23,29 @@ if (process.env.VERCEL !== '1') {
 
 // exporta o app para a Vercel usar como serverless function
 export default app;
+
+const alunos = await prisma.aluno.findMany({
+  select: {
+    id: true,
+    nome: true,
+    email: true,
+    cidade: true,
+    frase: true,
+    planosFuturos: true,
+    fotoUrl: true,
+    role: true,
+    criadoEm: true,
+    // senhaHash: NÃO está aqui — nunca retornado
+  },
+});
+
+const mensagens = await prisma.mensagem.findMany({
+  include: {
+    autor: {
+      select: {
+        nome: true,
+        fotoUrl: true,
+      },
+    },
+  },
+});
