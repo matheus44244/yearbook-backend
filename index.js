54eef93d726d9c4986afac45a368da3112b490d5
Bernardo-Cardoso-vieira <bcv@aluno.ifnmg.edu.br>
2026-07-01 16:37:57 +0000

    refactor: organiza middlewares e estrutura de imports

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import express from 'express';                // importa o Express
+import express from 'express';   
+import logger from './middlewares/logger.js';               // importa o Express
 import alunosRouter from './routes/alunos.js'; // importa o router de alunos <- NOVO
 
 const app = express();      // cria a aplicação Express
 const PORT = 3000;          // porta do servidor
 
 app.use(express.json());    // middleware que parseia JSON do body das requisições  <- NOVO
-
+app.use(logger);   
 // rota raiz — boas-vindas
 app.get('/', (req, res) => {
   res.json({ mensagem: 'Yearbook API está no ar! 🎓' });
